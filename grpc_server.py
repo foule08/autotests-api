940a4577a52e476e3acaa98f97ca83ecede7ca40
Foule <maly228123@gmail.com>
2026-08-02 17:16:21 +0300

    Домашка gRPC

--- grpc_server.py
+++ grpc_server.py
@@ -3,9 +3,9 @@ from concurrent import futures
 import user_service_pb2
 import user_service_pb2_grpc
 
-class UserServiceServicer(user_service_pb2_grpc.UserServiceServicer):
-    def GetUser(self, request, context):
-        print(f'Получен запрос к методу GetUser от пользователя: {request.username}')
+class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
+    def GetCourse(self, request, context):
+        print(f'Получен запрос к методу GetCourse от пользователя: {request.username}')
         return user_service_pb2.GetUserResponse(message=f'Привет, {request.username}')
 
 def serve():
